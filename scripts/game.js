336ba5be07c1df78821a78927af578486918919a
Александр Сыромятников <ya.sanek0808ya-sanek0808@outlook.com>
2025-05-09 19:21:41 +0300

    create game "Snake"

---
+++ scripts/game.js
@@ -0,0 +1,8 @@
+// Элементы DOM
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const startMenu = document.getElementById('startMenu');
+const startButton = document.getElementById('startButton');
+const constrolsButton = document.getElementById('controlsButton');
+const difficultySelect = document.getElementById('difficultySelect');
+const highScopeDisplay = document.getElementById('highScopeDisplay');
